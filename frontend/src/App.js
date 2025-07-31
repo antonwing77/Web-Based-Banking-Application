9339342493218f8606193363e113a609c06fa88d
antonwing77 <114882878+antonwing77@users.noreply.github.com>
2025-07-30 21:43:05 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
 import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import Transactions from './components/Transactions';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path='/'        element={<Welcome />}  />
           <Route path='/login'   element={<Login />}    />
           <Route path='/register' element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
         </Routes>
       </Router>
     </div>
